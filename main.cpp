cc77e37da25761965882650612620c2230c3b3cd
RatchaphonHeart <132637792+RatchaphonHeart@users.noreply.github.com>
2023-05-26 05:07:52 +0000

    done

--- main.cpp
+++ main.cpp
@@ -5,29 +5,34 @@ using namespace std;
 //3. Take input from argv
 
 
-int main(int argc, char **argv){
- int i,n,*a;
-			n=argc-1;
- 		     a=new int[n];
-			for(i=0;i<n;i++) {
-			    a[i]=atoi(argv[i]);
-		       cout<< "a[" << i << "]=" <<a[i]<<endl;
-		}
+int main(int argc, char **argv) {
+  int n, *a;
+  n = argc - 1;
+  a = new int[n];  
+ 
+  for (int i = 0; i < n; i++) {
+      a[i] = atoi(argv[i + 1]);
+      cout << "a[" << i << "]=" << a[i] << endl;
+  }
 
-    int*p=a;
-    int*pb;
+  int *p = a;
+  int *pb = a + n - 1;
+  int temp;
+  for (int i = 0; i < n / 2; i++) {
+      temp = *p;
+      *p = *pb;
+      *pb = temp;
+      p++;
+      pb--;
+  }
 
+    p = a;
 
-  
-     // Using p within your program 
-  for(i=0;i<n;i++) {
- 		   cout<<p[i]<<" "; //p is the location where p points to
- //     p++;  
- }
+    cout << "Reversed " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "a[" << i << "]=" << a[i] << endl;
+    }
 
- delete [] a;  
-}
-
-  
-//}
-
+    delete[] a;
+    return 0;
+} 
